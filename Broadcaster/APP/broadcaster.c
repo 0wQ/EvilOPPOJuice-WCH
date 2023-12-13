749d5fa3b94f5d9ac9c28549b7afd55cecf6fb18
Mizore <xvz@live.com>
2023-12-13 18:46:24 +0800

    Move broadcaster.c to /Broadcaster/APP folder

---
+++ Broadcaster/APP/broadcaster.c
@@ -0,0 +1,228 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : broadcaster.c
+ * Author             : WCH
+ * Version            : V1.0
+ * Date               : 2020/08/06
+ * Description        : 广播应用程序，初始化广播连接参数，然后处于广播态一直广播
+
+ *********************************************************************************
+ * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+ * Attention: This software (modified or not) and binary are used for
+ * microcontroller manufactured by Nanjing Qinheng Microelectronics.
+ *******************************************************************************/
+
+#include "CONFIG.h"
+#include "devinfoservice.h"
+#include "broadcaster.h"
+
+// 广播间隔 (units of 625us, min is 160=100ms)
+#define DEFAULT_ADVERTISING_INTERVAL 160 * 1
+// 间隔
+#define SBP_PERIODIC_EVT_PERIOD 1600 * 3
+
+// Task ID for internal task/event processing
+static uint8_t Broadcaster_TaskID;
+
+// 广播数据
+static uint8_t advertData[] = {
+    0x02,
+    0x01,
+    0x18,
+
+    0x16,
+    0xFF,
+    0x9A,
+    0x07,
+    0x10,
+    0x20,
+    0x06,
+    0x3F,
+    0x00,
+    0x01,
+    0x98,
+    0x00,
+    0xCD,
+    0x49,
+    0x42,
+    0xB3,
+    0xFD,
+    0x2C,
+    0x01,
+    0x51,
+    0x65,
+    0x3D,
+    0x01,
+};
+
+/*********************************************************************
+ * LOCAL FUNCTIONS
+ */
+static void Broadcaster_ProcessTMOSMsg(tmos_event_hdr_t* pMsg);
+static void Broadcaster_StateNotificationCB(gapRole_States_t newState);
+extern bStatus_t GAP_UpdateAdvertisingData(uint8_t taskID, uint8_t adType, uint16_t dataLen, uint8_t* pAdvertData);
+
+/*********************************************************************
+ * PROFILE CALLBACKS
+ */
+
+// GAP Role Callbacks
+static gapRolesBroadcasterCBs_t Broadcaster_BroadcasterCBs = {
+    Broadcaster_StateNotificationCB, // Profile State Change Callbacks
+    NULL
+};
+
+/*********************************************************************
+ * @fn      Broadcaster_Init
+ *
+ * @brief   Initialization function for the Broadcaster App
+ *          Task. This is called during initialization and should contain
+ *          any application specific initialization (ie. hardware
+ *          initialization/setup, table initialization, power up
+ *          notificaiton ... ).
+ *
+ * @param   task_id - the ID assigned by TMOS.  This ID should be
+ *                    used to send messages and set timers.
+ *
+ * @return  none
+ */
+void Broadcaster_Init()
+{
+    Broadcaster_TaskID = TMOS_ProcessEventRegister(Broadcaster_ProcessEvent);
+
+    // Setup the GAP Broadcaster Role Profile
+    {
+        // Device starts advertising upon initialization
+        uint8_t initial_advertising_enable = TRUE;
+        uint8_t initial_adv_event_type = GAP_ADTYPE_ADV_NONCONN_IND;
+
+        // Set the GAP Role Parameters
+        GAPRole_SetParameter(GAPROLE_ADVERT_ENABLED, sizeof(uint8_t), &initial_advertising_enable);
+        GAPRole_SetParameter(GAPROLE_ADV_EVENT_TYPE, sizeof(uint8_t), &initial_adv_event_type);
+        GAPRole_SetParameter(GAPROLE_ADVERT_DATA, sizeof(advertData), advertData);
+    }
+
+    // Set advertising interval
+    {
+        uint16_t advInt = DEFAULT_ADVERTISING_INTERVAL;
+
+        GAP_SetParamValue(TGAP_DISC_ADV_INT_MIN, advInt);
+        GAP_SetParamValue(TGAP_DISC_ADV_INT_MAX, advInt);
+    }
+
+    // Setup a delayed profile startup
+    tmos_set_event(Broadcaster_TaskID, SBP_START_DEVICE_EVT);
+
+    // 设置定时更新广播
+    tmos_start_task(Broadcaster_TaskID, SBP_PERIODIC_EVT, SBP_PERIODIC_EVT_PERIOD);
+}
+
+/*********************************************************************
+ * @fn      Broadcaster_ProcessEvent
+ *
+ * @brief   Broadcaster Application Task event processor. This
+ *          function is called to process all events for the task. Events
+ *          include timers, messages and any other user defined events.
+ *
+ * @param   task_id  - The TMOS assigned task ID.
+ * @param   events - events to process.  This is a bit map and can
+ *                   contain more than one event.
+ *
+ * @return  events not processed
+ */
+uint16_t Broadcaster_ProcessEvent(uint8_t task_id, uint16_t events)
+{
+    if (events & SYS_EVENT_MSG) {
+        uint8_t* pMsg;
+
+        if ((pMsg = tmos_msg_receive(Broadcaster_TaskID)) != NULL) {
+            Broadcaster_ProcessTMOSMsg((tmos_event_hdr_t*)pMsg);
+
+            // Release the TMOS message
+            tmos_msg_deallocate(pMsg);
+        }
+
+        // return unprocessed events
+        return (events ^ SYS_EVENT_MSG);
+    }
+
+    if (events & SBP_START_DEVICE_EVT) {
+        // Start the Device
+        GAPRole_BroadcasterStartDevice(&Broadcaster_BroadcasterCBs);
+
+        return (events ^ SBP_START_DEVICE_EVT);
+    }
+
+    if (events & SBP_PERIODIC_EVT) {
+        // advertData[25] 0x01 0x02 0x03 耳机颜色
+        if (advertData[25] == 0x03) {
+            advertData[25] = 0x01;
+        } else {
+            advertData[25] += 1;
+        }
+
+        // advertData[21] 0x00 0x01 开关耳机仓盖
+        if (advertData[21] == 0x00) {
+            advertData[21] = 0x01;
+            tmos_start_task(Broadcaster_TaskID, SBP_PERIODIC_EVT, SBP_PERIODIC_EVT_PERIOD);
+        } else {
+            advertData[21] = 0x00;
+            tmos_start_task(Broadcaster_TaskID, SBP_PERIODIC_EVT, SBP_PERIODIC_EVT_PERIOD / 2);
+        }
+        GAP_UpdateAdvertisingData(0, TRUE, sizeof(advertData), advertData);
+
+        return (events ^ SBP_PERIODIC_EVT);
+    }
+
+    // Discard unknown events
+    return 0;
+}
+
+/*********************************************************************
+ * @fn      Broadcaster_ProcessTMOSMsg
+ *
+ * @brief   Process an incoming task message.
+ *
+ * @param   pMsg - message to process
+ *
+ * @return  none
+ */
+static void Broadcaster_ProcessTMOSMsg(tmos_event_hdr_t* pMsg)
+{
+    switch (pMsg->event) {
+    default:
+        break;
+    }
+}
+
+/*********************************************************************
+ * @fn      Broadcaster_StateNotificationCB
+ *
+ * @brief   Notification from the profile of a state change.
+ *
+ * @param   newState - new state
+ *
+ * @return  none
+ */
+static void Broadcaster_StateNotificationCB(gapRole_States_t newState)
+{
+    switch (newState) {
+    case GAPROLE_STARTED:
+        PRINT("Initialized..\n");
+        break;
+
+    case GAPROLE_ADVERTISING:
+        PRINT("Advertising..\n");
+        break;
+
+    case GAPROLE_WAITING:
+        PRINT("Waiting for advertising..\n");
+        break;
+
+    case GAPROLE_ERROR:
+        PRINT("Error..\n");
+        break;
+
+    default:
+        break;
+    }
+}
